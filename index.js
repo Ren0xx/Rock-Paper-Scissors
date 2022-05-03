4b0e9e13365d7364a646a1262bed11ea04a30b1c
Adam Załęski <adamzaleski001@wp.pl>
2022-05-03 12:58:22 +0200

    Minor changes in functions, changing choices to id's, not textContent

--- index.js
+++ index.js
@@ -12,7 +12,35 @@ computerScore.textContent = 0;
 let playerCounter = 0;
 let computerCounter = 0;
 
-function stopGame(){
+function computerChoice(){
+    const myArray = ['ROCK', 'PAPER', 'SCISSORS'];
+    return myArray[Math.floor(Math.random() * myArray.length)];
+}
+function getResult(playerSelection, computerSelection){    
+    playerSelection = playerSelection.toUpperCase();
+    const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+
+    if ((playerSelection === computerSelection)) {
+        return result.textContent = "It's a draw.";
+    }    
+    if (playerSelection === choices[0]){       
+        return (computerSelection === choices[2]) ? 
+        (result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`, playerScore.textContent++, playerCounter++) 
+        :(result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`, computerScore.textContent++, computerCounter++);
+    }
+    if (playerSelection === choices[1]){
+        return (computerSelection === choices[0]) ? 
+        (result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`, playerScore.textContent++, playerCounter++) 
+        :(result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`, computerScore.textContent++, computerCounter++);
+    }
+    if (playerSelection === choices[2]){
+        return (computerSelection === choices[1]) ? 
+        (result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`, playerScore.textContent++, playerCounter++) 
+        :(result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`, computerScore.textContent++, computerCounter++);
+    }
+        
+}  
+function restartGame(){
     const restart = document.querySelector('.restart');
     const restartButton = document.createElement('button');
 
@@ -20,9 +48,9 @@ function stopGame(){
     restartButton.className = "button";
     restartButton.addEventListener('click', () =>{
         result.textContent = "Let's Play!";
-        document.getElementById(`btn1`).disabled = false; 
-        document.getElementById(`btn2`).disabled = false; 
-        document.getElementById(`btn3`).disabled = false;    
+        document.getElementById(`rock`).disabled = false; 
+        document.getElementById(`paper`).disabled = false; 
+        document.getElementById(`scissors`).disabled = false;    
         playerScore.textContent = 0, computerScore.textContent = 0;
         playerCounter = 0, computerCounter = 0;
         restartButton.remove();
@@ -31,67 +59,25 @@ function stopGame(){
     restart.append(restartButton);
 }
 
-function restart(){
-    if (computerCounter === 5 || playerCounter === 5){    
-        document.getElementById(`btn1`).disabled = true; 
-        document.getElementById(`btn2`).disabled = true; 
-        document.getElementById(`btn3`).disabled = true;    
-        stopGame();
-        return (computerCounter > playerCounter) ? result.textContent = `You've lost this one. Better luck next time!` :  result.textContent = `You've won this one. Congratulations!`;
-    }
-
+function stopGame(){   
+    
+    document.getElementById(`rock`).disabled = true; 
+    document.getElementById(`paper`).disabled = true; 
+    document.getElementById(`scissors`).disabled = true;    
+    restartGame();
+    return (computerCounter > playerCounter) ? result.textContent = `You've lost this one. Better luck next time!` :  result.textContent = `You've won this one. Congratulations!`;
+    
 }
-function playGame()
+function playGame(howManyGames = 5)
 {    
     buttons.forEach((button) => {
         button.addEventListener('click', () => {        
-            choice = button.firstChild.textContent;                
-            getWinner(choice, computerPlay());        
-            restart();                     
+            choice = button.id;                
+            getResult(choice, computerChoice());                  
+            if (parseInt(playerScore.textContent) === howManyGames || parseInt(computerScore.textContent) === howManyGames){
+                stopGame();                     
+            }
             });
     });     
 }
-function computerPlay(){
-    const myArray = ['ROCK', 'PAPER', 'SCISSORS'];
-    return myArray[Math.floor(Math.random() * myArray.length)];
-}
-function getWinner(playerSelection, computerPlay){
-    
-    playerSelection = playerSelection.toUpperCase();
-    const choices = ['ROCK', 'PAPER', 'SCISSORS'];
-    if ((playerSelection === computerPlay)) {
-        return result.textContent = "It's a draw";
-    }    
-    if (playerSelection === choices[0]){       
-        return (computerPlay === choices[2]) ? 
-        (result.textContent = `You win! ${playerSelection} beats ${computerPlay}`, playerScore.textContent++, playerCounter++) 
-        :(result.textContent = `You lose! ${computerPlay} beats ${playerSelection}`, computerScore.textContent++, computerCounter++);
-    }
-    if (playerSelection === choices[1]){
-        return (computerPlay === choices[0]) ? 
-        (result.textContent = `You win! ${playerSelection} beats ${computerPlay}`, playerScore.textContent++, playerCounter++) 
-        :(result.textContent = `You lose! ${computerPlay} beats ${playerSelection}`, computerScore.textContent++, computerCounter++);
-    }
-    if (playerSelection === choices[2]){
-        return (computerPlay === choices[1]) ? 
-        (result.textContent = `You win! ${playerSelection} beats ${computerPlay}`, playerScore.textContent++, playerCounter++) 
-        :(result.textContent = `You lose! ${computerPlay} beats ${playerSelection}`, computerScore.textContent++, computerCounter++);
-    }
-    
-    
-}    
-playGame();
-
-// Adding buttons through js, not html (will not be used here)
-// const btn1 = document.createElement('button');
-// const btn2 = document.createElement('button');
-// const btn3 = document.createElement('button');
-
-// btn1.textContent = 'Rock';
-// btn2.textContent = 'Paper';
-// btn3.textContent = 'Scissors';
-
-
-// container.appendChild(btn1);
-// container.appendChild(btn2);
-// container.appendChild(btn3);
+playGame(howManyGames = 5); //default value of games
